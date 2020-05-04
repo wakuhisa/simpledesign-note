9ecf4d84e79668d6a7606f4f8c416ae83e4bedb8
wakuhisa <1bpj2117@gmail.com>
2020-05-04 15:34:14 +0900

    edit devise user nickname unique

--- db/migrate/20200504054008_add_column_to_users.rb
+++ db/migrate/20200504054008_add_column_to_users.rb
@@ -1,5 +1,6 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :nickname, :string
+    add_index :users, :nickname, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_060343) do
     t.datetime "updated_at", null: false
     t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
