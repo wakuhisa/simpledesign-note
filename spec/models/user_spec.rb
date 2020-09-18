776004bbc2b1b253e6e1d49a238221cf9ebe9599
wakuhisa <1bpj2117@gmail.com>
2020-09-19 02:16:11 +0900

    edit user_spec.rb , it "重複したメールアドレスの場合、無効である"

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,21 @@ RSpec.describe User, type: :model do
     expect(user.valid?).to eq(false)
   end
 
+  it "重複したメールアドレスの場合、無効である" do
+    User.create(
+      email: "testman@example.com",
+      password: "password",
+      nickname: "testman"
+    )
+
+    @user = User.new(
+      email: "testman@example.com",
+      password: "password",
+      nickname: "exampleman"
+    )
+
+    @user.valid?
+    expect(@user.valid?).to eq(false)
+  end
+
 end
