cc77e688f4b74ab933bf3af68dc337d8f2a2ab2d
wakuhisa <1bpj2117@gmail.com>
2020-09-21 12:09:50 +0900

    edit factories/user.rb to factory_bot , name , sequence , password

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name { "test" }
-    sequence(:email) { |n| "Test#{n}@example.com" }
-    password { "testuser" }
+    nickname { "testman" }
+    sequence(:email) { |n| "testman#{n}@example.com" }
+    password { "password" }
   end
 end
