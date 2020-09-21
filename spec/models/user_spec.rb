45085ed7e50093d34c90f2dea6d6a8a903859cf8
wakuhisa <1bpj2117@gmail.com>
2020-09-21 12:13:59 +0900

    it "メール、パスワード、ニックネームがある場合、有効である" add FactoryBot.create

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   it "メール、パスワード、ニックネームがある場合、有効である" do
-    user = User.new(
-      email: "testman@example.com",
-      password: "password",
-      nickname: "testman"
-    )
-    expect(user).to be_valid
+    expect(FactoryBot.create(:user)).to be_valid
   end
 
   it "メールアドレスがない場合、無効である" do
